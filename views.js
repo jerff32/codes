70be2dbafad48c8c02b8a5259b51cccf7dd530b6
VulcanAsu <87685343+VulcanAsu@users.noreply.github.com>
2021-07-22 13:10:12 -0300

    Create views.js

---
+++ views.js
@@ -0,0 +1,87 @@
+( function($) {
+	$(function() {$(".elementor-widget-text-editor").html(function() {var vi = ' Visualizações esse ano';  var vz = 'ª ― com ';let zero = '~~';
+
+/*
+                 
+  ~ ~ ~ ~ ~ ~ TRADUÇÕES  ~ ~ ~ ~ ~ ~
+*/     
+
+// ~ ~ ~ The Legendary Mechanic
+var TML = '1' + vz + '1M' + vi;
+// ~ ~ ~ Monster Paradise
+var MP = '2' + vz + '390.519' + vi;
+// ~ ~ ~ Nascimento_da_Espada_Demoníaca
+var NED = '3' + vz + '309.940' + vi;
+// ~ ~ ~ The_Evolution_of_a_Goblin_to_the_Peak
+var EGP = '4' + vz + '206.367' + vi;
+// ~ ~ ~ My_Dragon_System
+var MDS = '5' + vz + '169.353' + vi;
+// ~ ~ ~ Nanomancer Reborn
+var NR = '6' + vz + '145.588' + vi;
+// ~ ~ ~ Monster Pet Evolution
+var MPE = '7' + vz + '101.758' + vi;
+// ~ ~ ~ Realm of Myths and Legends
+var RML = '8' + vz + '78.822' + vi;
+// ~ ~ ~ O Tutorial é Tão Difícil
+var TTD = '9' + vz + '78.161' + vi;  
+// ~ ~ ~ Esqueleto Nível 1
+var NV1 = '10' + vz + '51.965' + vi;
+// ~ ~ ~ Summoning the Holy Sword
+var SHS = '11' + vz + '51.026' + vi;
+// ~ ~ ~ O Mundo dos Sistemas
+var SBS = '12' + vz + '37.988' + vi;
+// ~ ~ ~ Mestre Caçador K
+var MCK = '13' + vz + '27.315' + vi;    
+// ~ ~ ~ O Sistema do Último Desejo
+var SUD = '14' + vz + '26.770' + vi;
+// ~ ~ ~ Sistema Íncubo
+var OSI = '15' + vz + '18.035' + vi;
+// ~ ~ ~ Exaltem o Orc!
+var PTO = '16' + vz + '17.829' + vi;
+// ~ ~ ~ Stop, Friendly Fire
+var SFF = '17' + vz + '17.542' + vi;
+// ~ ~ ~ Makai Hong        
+var Makai = '18' + vz + '15.143' + vi;
+// ~ ~ ~ Plague Doctor        
+var PD = '19' + vz + '14.642' + vi;
+// ~ ~ ~ Destino Subvertido        
+var DS = '20' + vz + '10.488' + vi;
+// ~ ~ ~ O Mago do Arco-Íris Retorna
+var MAI = '21' + vz + '9.102' + vi;
+// ~ ~ ~ Sistema de Tecnologia Avançada do Estudioso
+var STE = '21' + vz + '5.907' + vi;       
+// ~ ~ ~ Homicida Convocado
+var HC = '22' + vz + '3.598' + vi;
+// ~ ~ ~ Grimgar
+var Grimgar = '23' + vz + '3.415' + vi;
+//Upando Comendo
+var UC = '24' + vz + '2.69' + vi;
+// ~ ~ ~ Eu Sou o Unico Necromante
+var SUN = zero
+// ~ ~ ~ Eu Reencarnei em uma Maquina de Vendas
+var RMV = zero
+
+var OMO = zero
+var LGP = zero
+
+/*
+~ ~ ~ ~ ~ ~ ~ ORIGINAIS  ~ ~ ~ ~ ~ ~
+*/     
+var KOC = '1' + vz + '24.773' + vi;
+var Kalui = '2' + vz + '10.321' + vi;
+var RAC = '3' + vz + '5.337' + vi;
+var TOB = '4' + vz + '3.857' + vi;
+var CYR = '5' + vz + '3.307' + vi;
+var TR = '6' + vz + '3.256' + vi;
+var DT = '7' + vz + '2.654' + vi;
+var CE = '8' + vz + '2.137' + vi;
+var Jack = '9' + vz + '1.081' + vi;
+var ROC = '10' + vz + '1.039' + vi;
+var SWS = '11' + vz + '899' + vi;
+var Our = '13' + vz + '607' + vi;
+var LDD = '14' + vz + '624' + vi;
+// ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
+     return $(this).html().replace('style="color: rgba(255, 255, 255, 0);', 'style="color: #add7e1;').replace('id="VISIVEL"', 'id="OCULTO"').replace(/#TML#/, TML).replace(/#MDS#/, MDS).replace(/#MP#/, MP).replace(/#EGP#/, EGP).replace(/#NED#/, NED).replace(/#NR#/, NR).replace(/#NV1#/, NV1).replace(/#MPE#/, MPE).replace(/#RML#/, RML).replace(/#TTD#/, TTD).replace(/#SHS#/, SHS).replace(/#SBS#/, SBS).replace(/#PTO#/, PTO).replace(/#OSI#/, OSI).replace(/#SUD#/, SUD).replace(/#SFF#/, SFF).replace(/#PD#/, PD).replace(/#STE#/, STE).replace(/#MCK#/, MCK).replace(/#MAI#/, MAI).replace(/#HC#/, HC).replace(/#Grimgar#/, Grimgar).replace(/#SUN#/, SUN).replace(/#RMV#/, RMV).replace(/#UC#/, UC).replace(/#KOC#/, KOC).replace(/#Kalui#/, Kalui).replace(/#CYR#/, CYR).replace(/#TR#/, TR).replace(/#DT#/, DT).replace(/#RAC#/, RAC).replace(/#Jack#/, Jack).replace(/#ROC#/, ROC).replace(/#SWS#/, SWS).replace(/#TOB#/, TOB).replace(/#CE#/, CE).replace(/#Makai#/, Makai).replace(/#OUR#/, Our).replace(/#LDD#/, LDD).replace(/#OMO#/, OMO).replace(/#LGP#/, LGP)
+     
+     ;});});
+})(jQuery);

8a59899a3571ac4c09b729ecdd01ca8329559f4a
VulcanAsu <87685343+VulcanAsu@users.noreply.github.com>
2021-07-28 16:38:20 -0300

    Update and rename j.v8.js to j.v9.js

--- j.v9.js
+++ j.v9.js
@@ -1432,7 +1432,7 @@ Outlayer.defaults = {
   transitionDuration: '0.4s',
   hiddenStyle: {
     opacity: 0,
-    transform: 'scale(0.001)'
+    transform: 'scale(0.01)'
   },
   visibleStyle: {
     opacity: 1,

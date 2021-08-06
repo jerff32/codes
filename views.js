bffa1299540f228c62c492b52815dad258c3e70c
VulcanAsu <87685343+VulcanAsu@users.noreply.github.com>
2021-08-06 11:12:23 -0300

    Update views.js

--- views.js
+++ views.js
@@ -7,55 +7,55 @@
 */     
 
 // ~ ~ ~ The Legendary Mechanic
-var TML = '1' + vz + '1M' + vi;
+var TML = '1' + vz + '1.3M' + vi;
 // ~ ~ ~ Monster Paradise
-var MP = '2' + vz + '390.519' + vi;
+var MP = '2' + vz + '448.635' + vi;
 // ~ ~ ~ Nascimento_da_Espada_Demoníaca
-var NED = '3' + vz + '309.940' + vi;
+var NED = '3' + vz + '406.947' + vi;
 // ~ ~ ~ The_Evolution_of_a_Goblin_to_the_Peak
-var EGP = '4' + vz + '206.367' + vi;
+var EGP = '4' + vz + '238.135' + vi;
 // ~ ~ ~ My_Dragon_System
-var MDS = '5' + vz + '169.353' + vi;
+var MDS = '5' + vz + '188.185' + vi;
 // ~ ~ ~ Nanomancer Reborn
-var NR = '6' + vz + '145.588' + vi;
+var NR = '6' + vz + '160.132' + vi;
 // ~ ~ ~ Monster Pet Evolution
-var MPE = '7' + vz + '101.758' + vi;
+var MPE = '7' + vz + '121.843' + vi;
 // ~ ~ ~ Realm of Myths and Legends
-var RML = '8' + vz + '78.822' + vi;
+var RML = '8' + vz + '105.440' + vi;
 // ~ ~ ~ O Tutorial é Tão Difícil
-var TTD = '9' + vz + '78.161' + vi;  
+var TTD = '9' + vz + '98.337' + vi;  
 // ~ ~ ~ Esqueleto Nível 1
-var NV1 = '10' + vz + '51.965' + vi;
+var NV1 = '11' + vz + '55.136' + vi;
 // ~ ~ ~ Summoning the Holy Sword
-var SHS = '11' + vz + '51.026' + vi;
+var SHS = '10' + vz + '60.913' + vi;
 // ~ ~ ~ O Mundo dos Sistemas
-var SBS = '12' + vz + '37.988' + vi;
+var SBS = '12' + vz + '40.186' + vi;
 // ~ ~ ~ Mestre Caçador K
-var MCK = '13' + vz + '27.315' + vi;    
+var MCK = '13' + vz + '38.325' + vi;    
 // ~ ~ ~ O Sistema do Último Desejo
-var SUD = '14' + vz + '26.770' + vi;
+var SUD = '14' + vz + '37.494' + vi;
 // ~ ~ ~ Sistema Íncubo
-var OSI = '15' + vz + '18.035' + vi;
+var OSI = '16' + vz + '19.788' + vi;
 // ~ ~ ~ Exaltem o Orc!
-var PTO = '16' + vz + '17.829' + vi;
+var PTO = '15' + vz + '24.109' + vi;
 // ~ ~ ~ Stop, Friendly Fire
-var SFF = '17' + vz + '17.542' + vi;
+var SFF = '17' + vz + '18.788' + vi;
 // ~ ~ ~ Makai Hong        
-var Makai = '18' + vz + '15.143' + vi;
+var Makai = '18' + vz + '17+581' + vi;
 // ~ ~ ~ Plague Doctor        
-var PD = '19' + vz + '14.642' + vi;
+var PD = '19' + vz + '15.614' + vi;
 // ~ ~ ~ Destino Subvertido        
-var DS = '20' + vz + '10.488' + vi;
+var DS = '20' + vz + '19.956' + vi;
 // ~ ~ ~ O Mago do Arco-Íris Retorna
-var MAI = '21' + vz + '9.102' + vi;
+var MAI = '21' + vz + '10.874' + vi;
 // ~ ~ ~ Sistema de Tecnologia Avançada do Estudioso
-var STE = '21' + vz + '5.907' + vi;       
+var STE = '21' + vz + '6.272' + vi;       
 // ~ ~ ~ Homicida Convocado
-var HC = '22' + vz + '3.598' + vi;
+var HC = '22' + vz + '3.848' + vi;
 // ~ ~ ~ Grimgar
-var Grimgar = '23' + vz + '3.415' + vi;
+var Grimgar = '23' + vz + '4.193' + vi;
 //Upando Comendo
-var UC = '24' + vz + '2.69' + vi;
+var UC = '24' + vz + '1.027' + vi;
 // ~ ~ ~ Eu Sou o Unico Necromante
 var SUN = zero
 // ~ ~ ~ Eu Reencarnei em uma Maquina de Vendas
@@ -67,19 +67,19 @@ var LGP = zero
 /*
 ~ ~ ~ ~ ~ ~ ~ ORIGINAIS  ~ ~ ~ ~ ~ ~
 */     
-var KOC = '1' + vz + '24.773' + vi;
-var Kalui = '2' + vz + '10.321' + vi;
-var RAC = '3' + vz + '5.337' + vi;
-var TOB = '4' + vz + '3.857' + vi;
-var CYR = '5' + vz + '3.307' + vi;
-var TR = '6' + vz + '3.256' + vi;
-var DT = '7' + vz + '2.654' + vi;
-var CE = '8' + vz + '2.137' + vi;
-var Jack = '9' + vz + '1.081' + vi;
-var ROC = '10' + vz + '1.039' + vi;
-var SWS = '11' + vz + '899' + vi;
-var Our = '13' + vz + '607' + vi;
-var LDD = '14' + vz + '624' + vi;
+var KOC = '1' + vz + '31.519' + vi;
+var Kalui = '2' + vz + '13.730' + vi;
+var RAC = '4' + vz + '7.287' + vi;
+var TOB = '3' + vz + '8.503' + vi;
+var CYR = '6' + vz + '3.898' + vi;
+var TR = '7' + vz + '3.461' + vi;
+var DT = '8' + vz + '2.794' + vi;
+var CE = '5' + vz + '4.634' + vi;
+var Jack = '9' + vz + '1.420' + vi;
+var ROC = '10' + vz + '1.386' + vi;
+var SWS = '11' + vz + '1.185' + vi;
+var Our = '13' + vz + '893' + vi;
+var LDD = '12' + vz + '1.126' + vi;
 // ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
      return $(this).html().replace('style="color: rgba(255, 255, 255, 0);', 'style="color: #add7e1;').replace('id="VISIVEL"', 'id="OCULTO"').replace(/#TML#/, TML).replace(/#MDS#/, MDS).replace(/#MP#/, MP).replace(/#EGP#/, EGP).replace(/#NED#/, NED).replace(/#NR#/, NR).replace(/#NV1#/, NV1).replace(/#MPE#/, MPE).replace(/#RML#/, RML).replace(/#TTD#/, TTD).replace(/#SHS#/, SHS).replace(/#SBS#/, SBS).replace(/#PTO#/, PTO).replace(/#OSI#/, OSI).replace(/#SUD#/, SUD).replace(/#SFF#/, SFF).replace(/#PD#/, PD).replace(/#STE#/, STE).replace(/#MCK#/, MCK).replace(/#MAI#/, MAI).replace(/#HC#/, HC).replace(/#Grimgar#/, Grimgar).replace(/#SUN#/, SUN).replace(/#RMV#/, RMV).replace(/#UC#/, UC).replace(/#KOC#/, KOC).replace(/#Kalui#/, Kalui).replace(/#CYR#/, CYR).replace(/#TR#/, TR).replace(/#DT#/, DT).replace(/#RAC#/, RAC).replace(/#Jack#/, Jack).replace(/#ROC#/, ROC).replace(/#SWS#/, SWS).replace(/#TOB#/, TOB).replace(/#CE#/, CE).replace(/#Makai#/, Makai).replace(/#OUR#/, Our).replace(/#LDD#/, LDD).replace(/#OMO#/, OMO).replace(/#LGP#/, LGP)
      

99457f9034d1f05978e012c451503d5d4f665615
VulcanAsu <87685343+VulcanAsu@users.noreply.github.com>
2021-09-19 17:13:04 -0300

    Update and rename telas-de-sistema.v5.js to telas-de-sistema.v6.js

--- telas-de-sistema.v6.js
+++ telas-de-sistema.v6.js
@@ -141,14 +141,26 @@ $(document).ready(function() {
         var Esverdeado = '<div id="SYS0" class="Verde4"><hr id="DVBLU" class="Esverdeado" />';
         var Dragao = '<div id="SYS0" class="Draco"><hr id="DRG1" />';
         var Dragao_Fim = '<hr id="DRG2" /></div>';
-        var Yellow = '<div id="SYS0" class="Yellow wcm">'
-        
+        var Yellow = '<div id="SYS0" class="Yellow wcm">';
+        var Orange = '<div id="SYS0" class="Orange wcm">';
+        var Indigo = '<div id="SYS0" class="Indigo wcm">';
+       var Violet = '<div id="SYS0" class="Violet wcm">';
+       var Violeta  = '<div id="SYS0" class="Violeta wcm">';
+        var Laranja  = '<div id="SYS0" class="Laranja wcm">';
+         var Marrom = '<div id="SYS0" class="Marrom wcm">';
         var SROXO = '<div id="SYS0" class="COR ROXO"><hr id="DVZZ" />';
         $(this).html(texto.replace(/#01#/g, dv1).replace(/\[themoneytizer id=”35847-39″\]|\⇐ Capítulo Anterior|Próximo Capítulo \⇒|\[themoneytizer id=”35847-11″\]|\[themoneytizer id=”35847-6″\]|\[themoneytizer id=”35847-31″\]|\[themoneytizer id=”35847-20″\]|\[themoneytizer id=”35847-4″\]|\[themoneytizer id=”35847-1″\]|\[themoneytizer id=”35847-28″]|\[themoneytizer id=”35847-19″]|\[themoneytizer id=”35847-16″]|\[themoneytizer id=”35847-2″]|\[themoneytizer id=”35847-3″]/g, ' ').replace(/#envenenamento#/g, envenenamento).replace(/#sangramento#/g, sangr).replace(/#02#/g, dv2).replace(/#03#/g, dv3).replace(/#04#/g, dv4).replace(/#05#/g, dv5).replace(/#06#/g, dv6).replace(/#07#/g, dv7).replace(/#08#/g, dv8).replace(/#09#/g, dv9).replace(/#10#/g, dv10).replace(/#11#/g, dv11).replace(/#12#/g, dv12).replace(/#13#/g, dv13).replace(/#14#/g, dv14).replace(/#15#/g, dv15).replace(/#16#/g, dv16).replace(/#17#/g, dv17).replace(/#18#/g, dv18).replace(/#19#/g, dv19).replace(/#20#/g, dv20).replace(/#21#/g, dv21).replace(/#22#/g, dv22).replace(/#23#/g, dv23).replace(/#24#/g, dv24).replace(/#25#/g, dv25).replace(/#26#/g, dv26).replace(/#27#/g, dv27).replace(/#28#/g, dv28).replace(/#29#/g, dv29).replace(/#30#/g, dv30).replace(/#sistema#/g, st00).replace(/#sistema-fim#/g, st01).replace(/#sistema-escuro#/g, stt00).replace(/#sistema-cor#/g, stt01).replace(/#sistema-red#/g, stt02).replace(/#sistema-verde#/g, stt03).replace(/#31#/g, dv31).replace(/#32#/g, dv32).replace(/#33#/g, dv33).replace(/#34#/g, dv34).replace(/#35#/g, dv35).replace(/#36#/g, dv36).replace(/#37#/g, dv37).replace(/#38#/g, dv38).replace(/#39#/g, dv39).replace(/#40#/g, dv40).replace(/#41#/g, dv41).replace(/#sistema-sen#/g, stt04).replace(/#fim-sen#/g, st02).replace(/#escuro-fim#/g, st011).replace(/#sistema-azul#/g, azul).replace(/#azul-fim#|#over-fim#|#ouro-fim#|#vermelho-fim#|#esverdeado-fim#/g, '</div>').replace(/#sistema-over#/g, Over).replace(/#sistema-ouro#/g, Ouro).replace(/#sistema-vermelho#/g, Vermelho1).replace(/#sistema-esverdeado#/g, Esverdeado).replace(/\/">Capítulo Anterior<\/a>|">Capítulo Anterior<\/a>/g, '/" id="OCULTO">Capítulo Anterior</a>').replace(/\/">Próximo Capítulo<\/a>|">Próximo Capítulo<\/a>/g, '/" id="OCULTO">Próximo Capítulo</a>').replace(/#sistema-cell#/g, '<div id="sistema-celular"><div id="screen"><div id="tela-celular"><div class="txt">').replace(/#cell-fim#/g, '</div></div></div><div id="button"></div><div id="speaker"></div><div id="camera"></div></div>').replace(/#sistema-draco#/g, Dragao).replace(/#draco-fim#/g, Dragao_Fim).replace(/#sono#/g, sono).replace('NA »CENTRAL NOVEL«', ' ').replace(/Índice<\/a>/g, '<\/a>').replace(/#sistema-roxo#/g, SROXO)
             .replace(/#display#/g, "<div class='tela_s'><div class='bd1'></div><div class='bd4'></div><div class=txt>")
             .replace(/#display-ia#/g, '<div class="tela_s IA"><div class="bd1"></div><div class="bd4"></div><img class="IA_img" src="/wp-content/uploads/2021/08/ia.webp" alt="" width="200"><div class="txt">')
             .replace(/#display-fim#/g, dply2)
                      .replace(/#sistema-yellow#/g, Yellow)
+                     .replace(/#sistema-orange#/g, Orange)
+                     .replace(/#sistema-indigo#/g, Indigo)
+                      .replace(/#sistema-violet#/g, Violet)
+                      .replace(/#sistema-violeta#/g, Violeta)
+                      .replace(/#sistema-laranja#/g, Laranja)
+                     .replace (/#sistema-marrom#/g, Marrom)
+                     
 
 
 
